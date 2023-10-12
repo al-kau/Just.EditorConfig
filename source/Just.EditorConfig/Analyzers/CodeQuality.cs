fe622ad02919a4b90455c85d3ad029d541a33b51
alexander <116556921+al-kau@users.noreply.github.com>
2023-10-12 16:28:07 +0300

    update rule examples (#7)

--- source/Just.EditorConfig/Analyzers/CodeQuality.cs
+++ source/Just.EditorConfig/Analyzers/CodeQuality.cs
@@ -1,5 +1,7 @@
 ﻿
 // IDE0076: Invalid target '~F:Just.EditorConfig.Analyzers.CodeQuality.Fake' - no matching field named 'Fake'
+using System.Diagnostics.CodeAnalysis;
+
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Category", "Id: Title", Scope = "member", Target = "~F:Just.EditorConfig.Analyzers.CodeQuality.Fake")]
 // IDE0076: Invalid scope 'property'
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Category", "Id: Title", Scope = "property", Target = "~P:Just.EditorConfig.Analyzers.CodeQuality.IDE0076_1")]
@@ -18,6 +20,100 @@
 
 namespace Just.EditorConfig.Analyzers
 {
+    // IDE0051: Remove unused private member
+    // dotnet_diagnostic.IDE0051.severity = none
+
+    // Code with violations
+    internal class IDE0051_0
+    {
+        // IDE0051: Remove unused private members
+        private readonly int _fieldPrivate;
+        private int PropertyPrivate => 1;
+        private static int GetNumPrivate() => 1;
+    }
+
+    // Fixed code
+    internal class IDE0051_1
+    {
+        // No IDE0051
+        internal readonly int FieldInternal;
+        private readonly int _fieldPrivateUsed;
+        public int PropertyPublic => _fieldPrivateUsed;
+        private static int GetNumPrivateUsed() => 1;
+        internal static int GetNumInternal() => GetNumPrivateUsed();
+        public static int GetNumPublic() => GetNumPrivateUsed();
+    }
+
+    // IDE0052: Remove unread private member
+    // dotnet_diagnostic.IDE0052.severity = none
+
+    // Code with violations
+    internal class IDE0052_0
+    {
+        // IDE0052: Remove unread private members
+        private readonly int _field1;
+        private int _field2;
+        private int Property { get; set; }
+
+        public IDE0052_0()
+        {
+            _field1 = 0;
+        }
+
+        public void SetMethod()
+        {
+            _field2 = 0;
+            Property = 0;
+        }
+    }
+
+    // Fixed code
+    internal class IDE0052_1
+    {
+        public IDE0052_1()
+        {
+        }
+
+        public void SetMethod()
+        {
+        }
+    }
+
+    // IDE0064: Make struct fields writable
+    // dotnet_diagnostic.IDE0064.severity = none
+
+    // Code with violations
+    internal struct IDE0064_0
+    {
+        public readonly int Value;
+
+        public IDE0064_0(int value)
+        {
+            Value = value;
+        }
+
+        public void Test()
+        {
+            this = new IDE0064_0(5);
+        }
+    }
+
+    // Fixed code
+    internal struct IDE0064_1
+    {
+        public int Value;
+
+        public IDE0064_1(int value)
+        {
+            Value = value;
+        }
+
+        public void Test()
+        {
+            this = new IDE0064_1(5);
+        }
+    }
+
     internal class CodeQuality
     {
         // IDE0076: Remove invalid global 'SuppressMessageAttribute'
@@ -31,5 +127,49 @@ namespace Just.EditorConfig.Analyzers
         // IDE0077: Avoid legacy format target in global 'SuppressMessageAttribute'
 
         public int IDE0077;
+    }
+
+    // IDE0079: Remove unnecessary suppressio
+    // dotnet_remove_unnecessary_suppression_exclusions
+    // dotnet_diagnostic.IDE0079.severity = none
+
+    internal class IDE0079
+    {
+        // Necessary pragma suppression
+#pragma warning disable IDE0051 // IDE0051: Remove unused member
+        private static int UnusedMethod()
+        {
+            return 0;
+        }
+#pragma warning restore IDE0051
+
+        // IDE0079: Unnecessary pragma suppression
+#pragma warning disable IDE0051 // IDE0051: Remove unused member
+        private static int UsedMethod()
+        {
+            return 0;
+        }
+#pragma warning restore IDE0051
+
+        public static int PublicMethod()
+        {
+            return UsedMethod();
+        }
+    }
+
+    internal class IDE0079_0
+    {
+        // Necessary SuppressMessage attribute suppression
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+        private readonly int _unusedField;
+
+        // IDE0079: Unnecessary SuppressMessage attribute suppression
+        [SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+        private readonly int _usedField;
+
+        public int PublicMethod2()
+        {
+            return _usedField;
+        }
     }
 }
--- source/Just.EditorConfig/Analyzers/Style.cs
+++ source/Just.EditorConfig/Analyzers/Style.cs
@@ -9,7 +9,7 @@ using System.Text.Encodings;
 
 namespace Just.EditorConfig.Analyzers
 {
-    enum E
+    internal enum E
     {
         A,
         B
@@ -22,16 +22,16 @@ namespace Just.EditorConfig.Analyzers
         // dotnet_diagnostic.IDE0001.severity = none
 
         // IDE0001: 'System.IO.FileInfo' can be simplified to 'FileInfo'
-        System.IO.FileInfo fileIDE0001_0;
+        public System.IO.FileInfo IDE0001_0 = null!;
 
         // Fixed code
-        FileInfo fileIDE0001_1;
+        public FileInfo IDE0001_1 = null!;
 
 
         // IDE0002: Simplify member access
         // dotnet_diagnostic.IDE0002.severity = none
-        static void IDE0002() { }
-        static void IDE0002_0()
+        private static void IDE0002() { }
+        private static void IDE0002_0()
         {
             // IDE0002: 'C.M1' can be simplified to 'M1'
             Style.IDE0002();
@@ -44,10 +44,10 @@ namespace Just.EditorConfig.Analyzers
         // dotnet_diagnostic.IDE0004.severity = none
 
         // Code with violations
-        int IDE0004_0 = (int)0;
+        public int IDE0004_0 = (int)1;
 
         // Fixed code
-        int IDE0004_1 = 0;
+        public int IDE0004_1 = 1;
 
         // IDE0005: Remove unnecessary import
         // dotnet_diagnostic.IDE0005.severity = none
@@ -57,21 +57,41 @@ namespace Just.EditorConfig.Analyzers
         // dotnet_diagnostic.IDE0010.severity = none
 
         // Code with violations
-        int IDE0010_0(E e)
+        private static int IDE0010_0(E e, int i)
         {
             // IDE0010: Add missing cases
+            switch (i)
+            {
+                case 0:
+                    return 0;
+            }
+
             switch (e)
             {
                 case E.A:
                     return 0;
             }
 
-            return -1;
+            switch (e)
+            {
+                case E.A:
+                    return 0;
+                default:
+                    return -1;
+            }
         }
 
         // Fixed code
-        int IDE0010_1(E e)
+        private static int IDE0010_1(E e, int i)
         {
+            switch (i)
+            {
+                case 0:
+                    return 0;
+                default:
+                    break;
+            }
+
             switch (e)
             {
                 case E.A:
@@ -88,25 +108,25 @@ namespace Just.EditorConfig.Analyzers
         // dotnet_diagnostic.IDE0035.severity = none
 
         // Code with violations
-        void IDE0035_0()
+        private static void IDE0035_0()
         {
-            throw new System.Exception();
+            throw new NotImplementedException();
 
             // IDE0035: Remove unreachable code
             int v = 0;
         }
 
         // Fixed code
-        void IDE0035_1()
+        private static void IDE0035_1()
         {
-            throw new System.Exception();
+            throw new NotImplementedException();
         }
 
 
         // IDE0050: Convert anonymous type to tuple
         // dotnet_diagnostic.IDE0050.severity = none
 
-        void IDE0050()
+        private static void IDE0050()
         {
             // Code with violations
             var t0 = new { a = 1, b = 2 };
@@ -115,140 +135,51 @@ namespace Just.EditorConfig.Analyzers
             var t1 = (a: 1, b: 2);
         }
 
-
-        // IDE0051: Remove unused private member
-        // dotnet_diagnostic.IDE0051.severity = none
-
-        // Code with violations
-        class IDE0051_0
-        {
-            // IDE0051: Remove unused private members
-            private readonly int _fieldPrivate;
-            private int PropertyPrivate => 1;
-            private int GetNumPrivate() => 1;
-
-            // No IDE0051
-            internal readonly int FieldInternal;
-            private readonly int _fieldPrivateUsed;
-            public int PropertyPublic => _fieldPrivateUsed;
-            private int GetNumPrivateUsed() => 1;
-            internal int GetNumInternal() => GetNumPrivateUsed();
-            public int GetNumPublic() => GetNumPrivateUsed();
-        }
-
-        // Fixed code
-        class IDE0051_1
-        {
-            // No IDE0051
-            internal readonly int FieldInternal;
-            private readonly int _fieldPrivateUsed;
-            public int PropertyPublic => _fieldPrivateUsed;
-            private int GetNumPrivateUsed() => 1;
-            internal int GetNumInternal() => GetNumPrivateUsed();
-            public int GetNumPublic() => GetNumPrivateUsed();
-        }
-
-        // IDE0052: Remove unread private member
-        // dotnet_diagnostic.IDE0052.severity = none
-
-        // Code with violations
-        class IDE0052_0
-        {
-            // IDE0052: Remove unread private members
-            private readonly int _field1;
-            private int _field2;
-            private int Property { get; set; }
-
-            public IDE0052_0()
-            {
-                _field1 = 0;
-            }
-
-            public void SetMethod()
-            {
-                _field2 = 0;
-                Property = 0;
-            }
-        }
-
-        // Fixed code
-        class IDE0052_1
-        {
-            public IDE0052_1()
-            {
-            }
-
-            public void SetMethod()
-            {
-            }
-        }
-
-        // IDE0064: Make struct fields writable
-        // dotnet_diagnostic.IDE0064.severity = none
-
-        // Code with violations
-        struct IDE0064_0
-        {
-            public readonly int Value;
-
-            public IDE0064_0(int value)
-            {
-                Value = value;
-            }
-
-            public void Test()
-            {
-                this = new IDE0064_0(5);
-            }
-        }
-
-        // Fixed code
-        struct IDE0064_1
-        {
-            public int Value;
-
-            public IDE0064_1(int value)
-            {
-                Value = value;
-            }
-
-            public void Test()
-            {
-                this = new IDE0064_1(5);
-            }
-        }
-
         // IDE0070: Use 'System.HashCode.Combine'
         // dotnet_diagnostic.IDE0070.severity = none
 
-        class IDE0070
+
+        private class IDE0070
         {
-            public override int GetHashCode() => 0;
+            public override int GetHashCode()
+            {
+                return 0;
+            }
         }
 
-        class IDE0070_0 : IDE0070
+        private class IDE0070_0 : IDE0070
         {
-            int j;
+            public int J { get; set; }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+
+        private class IDE0070_1 : IDE0070
+        {
+            public int J { get; set; }
 
             // Code with violations
             public override int GetHashCode()
             {
                 // IDE0070: GetHashCode can be simplified.
-                var hashCode = 339610899;
-                hashCode = hashCode * -1521134295 + base.GetHashCode();
-                hashCode = hashCode * -1521134295 + j.GetHashCode();
+                int hashCode = 339610899;
+                hashCode = (hashCode * -1521134295) + base.GetHashCode();
+                hashCode = (hashCode * -1521134295) + J.GetHashCode();
                 return hashCode;
             }
         }
 
-        class IDE0070_1 : IDE0070
+        private class IDE0070_2 : IDE0070
         {
-            int j;
+            public int J { get; set; }
 
             // Fixed code
             public override int GetHashCode()
             {
-                return System.HashCode.Combine(base.GetHashCode(), j);
+                return HashCode.Combine(base.GetHashCode(), J);
             }
         }
 
@@ -257,7 +188,7 @@ namespace Just.EditorConfig.Analyzers
         // IDE0072: Add missing cases to switch expression
 
         // Code with violations
-        int IDE0072_0(E e)
+        private static int IDE0072_0(E e)
         {
             // IDE0072: Add missing cases
             return e switch
@@ -267,8 +198,18 @@ namespace Just.EditorConfig.Analyzers
             };
         }
 
+        private static int IDE0072_1(int i)
+        {
+            return i switch
+            {
+                -1 => 0,
+                1 => 0,
+                int.MaxValue or int.MaxValue => 0,
+            };
+        }
+
         // Fixed code
-        int IDE0072_1(E e)
+        private static int IDE0072_2(E e)
         {
             return e switch
             {
@@ -278,61 +219,99 @@ namespace Just.EditorConfig.Analyzers
             };
         }
 
+        private static int IDE0072_3(E e)
+        {
+            return e switch
+            {
+                E.A or E.B => 0,
+                _ => -1,
+            };
+        }
+
+        private static int IDE0072_4(int i)
+        {
+            return i switch
+            {
+                -1 or 1 => 0,
+                int.MaxValue or int.MaxValue => 0,
+                _ => -1,
+            };
+        }
+
 
         // IDE0080: Remove unnecessary suppression operator
         // dotnet_diagnostic.IDE0080.severity = none
 
-        void IDE0080(object o)
+        private static void IDE0080(object? o)
         {
             // Code with violations
-            if (o !is string) { }
+            if (o !is string)
+            { }
 
             // Potential fixes:
             // 1.
-            if (o is not string) { }
+            if (o is not string)
+            { }
 
             // 2.
-            if (!(o is string)) { }
+            if (!(o is string))
+            { }
 
             // 3.
-            if (o is string) { }
+            if (o is string)
+            { }
         }
 
         // IDE0082: Convert 'typeof' to 'nameof'
         // dotnet_diagnostic.IDE0082.severity = none
 
-        void IDE0082<T>()
+        private static void IDE0082<T>()
         {
             // Code with violations
-            var n00 = typeof(T).Name;
-            var n01 = typeof(int).Name;
+            string n00 = typeof(T).Name;
+            string n01 = typeof(int).Name;
 
             // Fixed code
-            var n10 = nameof(T);
-            var n11 = nameof(Int32);
+            string n10 = nameof(T);
+            string n11 = nameof(Int32);
         }
 
         // IDE0100: Remove unnecessary equality operator
         // dotnet_diagnostic.IDE0100.severity = none
 
-        void IDE0100(bool x)
+        private static void IDE0100(bool x, bool? xx)
         {
-            static bool M() { return false; }
+            static bool M()
+            {
+                return false;
+            }
 
             // Code with violations
-            if (x == true) { }
-            if (M() != false) { }
+            if (x == true)
+            { }
+
+            if (M() != false)
+            { }
 
             // Fixed code
-            if (x) { }
-            if (M()) { }
+            if (x)
+            { }
+
+            if (M())
+            { }
+
+            if (xx != false)
+            { }
+
+            if (xx == true)
+            { }
         }
 
 
         // IDE0110: Remove unnecessary discard
         // dotnet_diagnostic.IDE0110.severity = none
 
-        void IDE0110(object o)
+        private static void IDE0110(object o)
         {
             // Code with violations
             switch (o)
@@ -342,6 +321,8 @@ namespace Just.EditorConfig.Analyzers
                     break;
                 case string _:
                     Console.WriteLine("Value was a string");
+                    break;
+                default:
                     break;
             }
 
@@ -354,31 +335,33 @@ namespace Just.EditorConfig.Analyzers
                 case string:
                     Console.WriteLine("Value was a string");
                     break;
+                default:
+                    break;
             }
         }
 
         // IDE0120: Simplify LINQ expression
         // dotnet_diagnostic.IDE0120.severity = none
 
-        void IDE0120()
+        private static void IDE0120()
         {
             // Code with violations.
             IEnumerable<string> words0 = new List<string> { "hello", "world", "!" };
-            var result0 = words0.Where(x => x.Equals("hello")).Any();
+            bool result0 = words0.Where(x => x.Equals("hello", StringComparison.Ordinal)).Any();
 
             // Fixed code.
             IEnumerable<string> words1 = new List<string> { "hello", "world", "!" };
-            var result1 = words1.Any(x => x.Equals("hello"));
+            bool result1 = words1.Any(x => x.Equals("hello", StringComparison.Ordinal));
         }
 
 
         // IDE0240: Nullable directive is redundant
         // dotnet_diagnostic.IDE0240.severity = none
 
-        void IDE0240()
+        private static void IDE0240()
         {
             // Code with violations (in a project with <Nullable>enable</Nullable>).
-            #nullable enable
+#nullable enable
             Console.WriteLine("Hello world.");
 
             // Fixed code.
@@ -388,14 +371,20 @@ namespace Just.EditorConfig.Analyzers
         // IDE0241: Nullable directive is unnecessary
         // dotnet_diagnostic.IDE0241.severity = none
 
-        void IDE0241()
+        // Code with violations (in a project with <Nullable>enable</Nullable>).
+#nullable disable
+        private enum IDE0241
         {
-            // Code with violations (in a project with <Nullable>enable</Nullable>).
-            #nullable disable
-            Console.WriteLine("Hello world.");
+            First,
+            Second
+        }
+#nullable enable
 
-            // Fixed code.
-            Console.WriteLine("Hello world.");
+        // Fixed code.
+        private enum IDE0241_1
+        {
+            First,
+            Second
         }
 
 
@@ -403,10 +392,10 @@ namespace Just.EditorConfig.Analyzers
         // dotnet_diagnostic.IDE0280.severity = none
 
         // Code with violations.
-        void IDE0280_0([NotNullIfNotNull("input")] string? input) { }
+        private static void IDE0280_0([NotNullIfNotNull("input")] string? input) { }
 
         // Fixed code.
-        void IDE0280_1([NotNullIfNotNull(nameof(input))] string? input) { }
+        private static void IDE0280_1([NotNullIfNotNull(nameof(input))] string? input) { }
 
         // IDE1006: Naming styles
         // dotnet_diagnostic.IDE1006.severity = none
